4ebff4b9094543020b4569afcda943022cb03279
Abhik Ray <abhik.raya01@gmail.com>
2026-03-10 21:24:55 +0530

    Revamp

--- src/NewApp.tsx
+++ src/NewApp.tsx
@@ -1,63 +1,81 @@
 import { GoogleGenAI } from "@google/genai";
 import { JobPreferenceData } from "./data/JobPreferenceData";
-import { Loader2 } from "lucide-react";
+import { Loader, Loader2 } from "lucide-react";
 import { judgeJobPosting, type JudgeResponseType } from "./utils/AIResumeJudge";
 import { useState } from "react";
 import { verifyApiKey } from "./utils/AIHealthCheck";
 import { JudgeResponse } from "./JudgeResponse";
+import { Button } from "./components/ui/button";
+import { generateResumeSection } from "./utils/AIResumeBuilder";
+import { userData } from "./data/UserData";
+import { SummaryData } from "./data/SummaryData";
+import { SkillsData } from "./data/SkillsData";
+import { MainExperienceData } from "./data/MainExperienceData";
+import { MyDocument } from "./pdf";
+import { PDFViewer } from "@react-pdf/renderer";
+import { Stepper } from "./components/ui/stepper";
 
 const steps = [
-  { no: 1, name: "Enter Job Description" },
-  { no: 2, name: "Job Review" },
-  { no: 3, name: "Resume Rebuild" },
-] as const;
+  { no: 0, title: "Job Description", description: "Paste Job Desription" },
+  {
+    no: 1,
+    title: "Job Review",
+    description: "AI will review the job based on your parameters",
+  },
+  {
+    no: 2,
+    title: "Resume Rebuild",
+    description: "Rebuild Resume related to the required parameters",
+  },
+];
 
 // type StepNumbers = (typeof steps)[number]["no"];
 
-const sampleJudgeResponse = {
-  overallMatchScore: 30,
-  matchAnalysis:
-    "This role aligns well with your desired Front End Developer role and core tech stack, particularly React and JavaScript. However, the absence of any mention of remote work is a significant deal-breaker, as your preference is for a fully remote position. Salary information is also missing.",
-  pros: [
-    "The role is a direct match for 'Front End Developer' and 'Software Developer' desired roles.",
-    "Strong alignment with core tech stack, specifically React and JavaScript.",
-    "Opportunity to work with the MERN stack (MongoDB, Express.js, React, Node.js).",
-    "Responsibilities include developing new user-facing features and building reusable components.",
-  ],
-  cons: [
-    "The job description does not mention remote work, which is a deal-breaker for your preferred work model.",
-    "Salary information is not provided, making it impossible to assess against your target.",
-    "Specific tech like React Native, Next.js, and Typescript are not explicitly mentioned as requirements.",
-    "The role is not in game development, which is one of your desired areas.",
-  ],
-  isRemote: ["On Site"],
-  redFlags: [
-    "No mention of remote work, which is a deal-breaker for the candidate.",
-    "The phrase 'fast-paced environment' often implies high pressure and potential for overwork.",
-    "Expectations to 'Provide technical guidance and mentorship to junior developers' and 'Lead code reviews' with only '3+ years of professional experience' might indicate high expectations for the experience level.",
-  ],
-  verdict: "Hard Pass",
-};
+// const sampleJudgeResponse = {
+//   overallMatchScore: 30,
+//   matchAnalysis:
+//     "This role aligns well with your desired Front End Developer role and core tech stack, particularly React and JavaScript. However, the absence of any mention of remote work is a significant deal-breaker, as your preference is for a fully remote position. Salary information is also missing.",
+//   pros: [
+//     "The role is a direct match for 'Front End Developer' and 'Software Developer' desired roles.",
+//     "Strong alignment with core tech stack, specifically React and JavaScript.",
+//     "Opportunity to work with the MERN stack (MongoDB, Express.js, React, Node.js).",
+//     "Responsibilities include developing new user-facing features and building reusable components.",
+//   ],
+//   cons: [
+//     "The job description does not mention remote work, which is a deal-breaker for your preferred work model.",
+//     "Salary information is not provided, making it impossible to assess against your target.",
+//     "Specific tech like React Native, Next.js, and Typescript are not explicitly mentioned as requirements.",
+//     "The role is not in game development, which is one of your desired areas.",
+//   ],
+//   isRemote: ["On Site"],
+//   redFlags: [
+//     "No mention of remote work, which is a deal-breaker for the candidate.",
+//     "The phrase 'fast-paced environment' often implies high pressure and potential for overwork.",
+//     "Expectations to 'Provide technical guidance and mentorship to junior developers' and 'Lead code reviews' with only '3+ years of professional experience' might indicate high expectations for the experience level.",
+//   ],
+//   verdict: "Hard Pass",
+// };
 
 export const NewApp = () => {
   const [geminiKey, setGeminiKey] = useState<string>("");
   const [jobDescription, setJobDescription] = useState("");
-  const [step, setStep] = useState<(typeof steps)[number]>(steps[1]);
+  const [step, setStep] = useState<number>(0);
   const [judgeResponse, setJudgeResponse] =
-    useState<JudgeResponseType>(sampleJudgeResponse);
-  const [judgement, setJudgement] = useState<boolean | null>(null);
+    useState<JudgeResponseType>();
   const [stepError, setStepError] = useState<string | null>(null);
   const [aiCore, setAICore] = useState<GoogleGenAI | null>(null);
-  // const aiCore = config.GeminiKey
-  //     ? new GoogleGenAI({ apiKey: config.GeminiKey })
-  //     : null;
+  const [isJudgementLoading, setIsJudgementLoading] = useState<boolean>(false);
+  const [isPdfLoading, setIsPdfLoading] = useState<boolean>(false);
+  const [summaryData, setSummaryData] = useState(SummaryData);
+  const [experienceData, setExperienceData] =
+    useState<string[]>(MainExperienceData);
+  const [skillsData, setSkillsData] = useState(SkillsData);
 
   console.log({
     geminiKey,
     jobDescription,
     step,
     judgeResponse,
-    judgement,
     stepError,
   });
 
@@ -83,11 +101,13 @@ export const NewApp = () => {
       return;
     }
 
+    setIsJudgementLoading(true);
+
     const aiCoreObject = await initializeAICore(geminiKey);
 
     setAICore(aiCoreObject);
 
-    if (!aiCore) {
+    if (!aiCoreObject) {
       setStepError("Couldn't initialize aiCore");
       return;
     }
@@ -96,7 +116,9 @@ export const NewApp = () => {
       await judgeJobPosting(aiCoreObject, jobDescription, JobPreferenceData),
     );
 
-    setStep(steps[1]);
+    setIsJudgementLoading(false);
+
+    setStep(1);
   };
 
   const isHealthy = async (): Promise<boolean> => {
@@ -106,15 +128,67 @@ export const NewApp = () => {
       .finally(() => false);
   };
 
+  const genarateSummary = async () => {
+    if (!aiCore) {
+      setStepError("Could not initialize Gemini");
+      return;
+    }
+    return generateResumeSection(aiCore, "summary", jobDescription, userData);
+  };
+
+  const genarateExperienceData = async () => {
+    if (!aiCore) {
+      setStepError("Could not initialize Gemini");
+      return;
+    }
+    return generateResumeSection(
+      aiCore,
+      "experience",
+      jobDescription,
+      userData,
+    );
+  };
+
+  const genarateSkills = async () => {
+    if (!aiCore) {
+      setStepError("Could not initialize Gemini");
+      return;
+    }
+    return generateResumeSection(aiCore, "skills", jobDescription, userData);
+  };
+
+  const onGenerateClick = async () => {
+    if (!aiCore) {
+      setStepError("Could not initialize Gemini");
+      return;
+    }
+    setIsPdfLoading(true);
+    const summary = await genarateSummary();
+    const skills = await genarateSkills();
+    const experience = await genarateExperienceData();
+
+    setSummaryData(summary["summary"]);
+    setSkillsData(skills?.["technicalSkills"]);
+    setExperienceData(experience["bulletPoints"]);
+    setIsPdfLoading(false);
+
+    setStep(2);
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-screen">
+    <div className="flex flex-col justify-center items-center w-screen overflow-y-auto">
       <div className="flex flex-col w-xl gap-4">
+        <Stepper
+          steps={steps}
+          currentStep={step}
+          onStepChange={setStep}
+        />
         {stepError && (
           <div className="border border-red-600 text-red-400 bg-red-50 rounded-2xl text-xs p-2">
             {stepError}
           </div>
         )}
-        {step.no === 1 && (
+        {step === 0 && (
           <>
             <div className="flex flex-col border border-blue-700 rounded-3xl">
               <label
@@ -148,26 +222,27 @@ export const NewApp = () => {
                 value={jobDescription}
               />
             </div>
-            <button
+            <Button
               className="p-2 bg-blue-700 text-white rounded-3xl cursor-pointer"
               onClick={onJudgeClick}
+              disabled={isJudgementLoading}
             >
-              Judge the job ⚖️
-            </button>
+              {!isJudgementLoading ? 'Judge the job ⚖️' : <>Judging... <Loader2 className="animate-spin"/></>}
+            </Button>
           </>
         )}
       </div>
-      {step.no === 2 && (
+      {step === 1 && (
         <>
           <div className="flex flex-col justify-center w-full">
             {judgeResponse ? (
               <div className="flex flex-col justify-center items-center w-full">
-                <JudgeResponse judgeResponse={sampleJudgeResponse} />
-                <button className=" w-50 p-2 bg-blue-700 text-white rounded-3xl cursor-pointer">
-                  {judgeResponse.overallMatchScore > 70
+                <JudgeResponse judgeResponse={judgeResponse} />
+                <Button className=" w-50 p-2 bg-blue-700 text-white rounded-3xl cursor-pointer" onClick={onGenerateClick} disabled={isPdfLoading}>
+                  {!isPdfLoading ? judgeResponse.overallMatchScore > 70
                     ? "Proceed with Resume"
-                    : "Still proceed with resume"}
-                </button>
+                    : "Still proceed with resume" : 'PDF is Loading'}
+                </Button>
               </div>
             ) : (
               <Loader2 className="animate-spin" />
@@ -175,7 +250,23 @@ export const NewApp = () => {
           </div>
         </>
       )}
-      {step.no === 3 && <div></div>}
+      {step === 2 && (
+        <div className="w-screen h-screen">
+          {!isPdfLoading ? (
+            <PDFViewer
+              className={`h-full w-full ${isPdfLoading ? "hidden" : ""}`}
+            >
+              <MyDocument
+                summaryData={summaryData}
+                mainExperienceData={experienceData}
+                skillsData={skillsData}
+              />
+            </PDFViewer>
+          ) : (
+            <Loader className="animate-spin" />
+          )}
+        </div>
+      )}
     </div>
   );
 };
