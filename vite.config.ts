24aa463b0155a3c4596a15da5c27b5933d9bef03
Abhik Ray <41513173+Abhik-Ray@users.noreply.github.com>
2026-03-11 11:58:46 +0530

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'Abhik-Ray/resume-builder',
+  base: 'resume-builder',
   plugins: [
     react({
       babel: {
